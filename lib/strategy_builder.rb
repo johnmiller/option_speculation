31932ffa9f9fbc0d09dec061cf36725ac256b2fc
John Miller <john2021@gmail.com>
2013-04-13 19:28:58 -0400

    removed stale functionalitu

--- lib/strategy_builder.rb
+++ lib/strategy_builder.rb
@@ -5,28 +5,6 @@ class StrategyBuilder
     @db = DataSession.new settings
   end
 
-  def find_current_best_picks
-    find_best_picks "20120306"
-  end
-
-  def find_best_picks(date)
-    #download_latest
-    @db.rebuild_database
-    #import date
-    #calculate_20day_moving_average
-  end
-
-  def download_latest()
-    return if !@settings["download_latest"]
-    file_grabber = FileGrabber.new @settings
-    file_grabber.download_new_options
-  end
-
-  def import(date)
-    file_importer = FileImporter.new @settings
-    file_importer.import date, @settings["num_of_days"]
-  end
-
   def process_xxx
     # download missing
     # extract new files to import dir
